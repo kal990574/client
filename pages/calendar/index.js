380fa1ec38fd973e394982de2f45fc1e694429cf
kitez <duswl339@ajou.ac.kr>
2023-03-27 17:54:14 +0900

    feat: calendar-page comment out

--- pages/calendar/index.js
+++ pages/calendar/index.js
@@ -8,37 +8,40 @@ import MonthView from "../../components/view/month-view/MonthView";
 import WeekView from "../../components/view/week-view/WeekView";
 import DayView from "../../components/view/day-view/DayView";
 
-function RenderingViewType({viewType}) {
-    if(viewType === 0) {
-        return <MonthView />
-    } else if(viewType === 1) {
-        return <WeekView />
-    } else {
-        return <DayView />
-    }
-}
+// function RenderingViewType({viewType}) {
+//     if(viewType === 0) {
+//         return <MonthView />
+//     } else if(viewType === 1) {
+//         return <WeekView />
+//     } else {
+//         return <DayView />
+//     }
+// }
 
 export default function Calendar() {
-    const [open, setOpen] = useState(false);
-    const [viewType, setViewType] = useState(0);
-
-    useEffect(() => {
-
-    }, [viewType]);
-
-    const onClickView = (event) => {
-        if(open) {
-            setOpen(false);
-        }
-    }
-
-    return(
-        <main className={styles.container} onClick={onClickView}>
-            <CustomHead title={'Calendar'} content={'CalendarPage'}/>
-            <NavigationTop open={open} setOpen={setOpen} viewType={viewType} setViewType={setViewType} />
-            <FriendsCircleList />
-            <RenderingViewType viewType={viewType} />
-            <NavigationBottom />
-        </main>
+    return (
+        <div>calendar</div>
     );
+    // const [open, setOpen] = useState(false);
+    // const [viewType, setViewType] = useState(0);
+    //
+    // useEffect(() => {
+    //
+    // }, [viewType]);
+    //
+    // const onClickView = (event) => {
+    //     if(open) {
+    //         setOpen(false);
+    //     }
+    // }
+    //
+    // return(
+    //     <main className={styles.container} onClick={onClickView}>
+    //         <CustomHead title={'Calendar'} content={'CalendarPage'}/>
+    //         <NavigationTop open={open} setOpen={setOpen} viewType={viewType} setViewType={setViewType} />
+    //         <FriendsCircleList />
+    //         <RenderingViewType viewType={viewType} />
+    //         <NavigationBottom />
+    //     </main>
+    // );
 }
