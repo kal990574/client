dac85b4ced3d81eff9442d06bec076beb4133042
kitez <duswl339@ajou.ac.kr>
2023-03-27 11:19:43 +0900

    feat: add FriendsCircleList Dummy data

---
+++ common/dummy.js
@@ -0,0 +1,13 @@
+const FRIENDS_LIST = [
+    { src: './img.png', title: '나' },
+    { src: './img.png', title: '이민서' },
+    { src: './img.png', title: '임수진' },
+    { src: './img.png', title: '문채원' },
+    { src: './img.png', title: '이재현' },
+    { src: './img.png', title: '손진혁' },
+    { src: './img.png', title: '박소은' },
+    { src: './img.png', title: '전주현' },
+    { src: './img.png', title: '유정협' },
+]
+
+export default FRIENDS_LIST;
