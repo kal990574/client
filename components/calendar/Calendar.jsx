939dc80643cbfa5d44b066382769826945d1fb10
kitez <duswl339@ajou.ac.kr>
2023-03-28 11:31:24 +0900

    feat: add Days component in Calendar component

--- components/calendar/Calendar.jsx
+++ components/calendar/Calendar.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Calendar.module.css';
 import MonthView from "../view/month-view/MonthView";
 import WeekView from "../view/week-view/WeekView";
 import DayView from "../view/day-view/DayView";
+import DateSelector from "../date-selector/DateSelector";
+import Days from "./days/Days";
 
-export default function Calendar({viewType, viewContent}) {
-    if(viewType === 0) {
-        return (
-            <MonthView viewContent={viewContent} />
-        )
-    } else if(viewType === 1) {
-        return (
-            <WeekView viewContent={viewContent} />
-        )
-    } else {
-        return (
-            <DayView viewContent={viewContent} />
-        )
-    }
+export default function Calendar({viewType}) {
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [viewContent, setViewContent] = useState(true); // ture: calendar, false: diary
+
+    return (
+        <>
+            <DateSelector viewContent={viewContent} setViewContent={setViewContent} selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+            { viewType === 0 || viewType === 1 ? <Days /> : <></> }
+            {
+                viewType === 0 ? <MonthView viewContent={viewContent} />
+                    : viewType === 1 ? <WeekView viewContent={viewContent} />
+                    : <DayView viewContent={viewContent} />
+            }
+
+        </>
+    )
 }
