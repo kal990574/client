7cd4e478d3e8626a9e0e0da0edb8846d3bf14d63
kitez <duswl339@ajou.ac.kr>
2023-03-29 13:11:50 +0900

    feat: modify Calendar structure using if and array

--- components/calendar/Calendar.jsx
+++ components/calendar/Calendar.jsx
@@ -5,22 +5,27 @@ import WeekView from "../view/week-view/WeekView";
 import DayView from "../view/day-view/DayView";
 import DateSelector from "../date-selector/DateSelector";
 import Days from "./days/Days";
+import DetailScheduleModal from "../modal/detail-schedule-modal/DetailScheduleModal";
 
 export default function Calendar({viewType}) {
     const [currentDate, setCurrentDate] = useState(new Date());
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [viewContent, setViewContent] = useState(true); // ture: calendar, false: diary
+    const [open, setOpen] = useState();
 
-    return (
-        <>
-            <DateSelector viewContent={viewContent} setViewContent={setViewContent} selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
-            { viewType === 0 || viewType === 1 ? <Days /> : <></> }
-            {
-                viewType === 0 ? <MonthView viewContent={viewContent} currentDate={currentDate} />
-                    : viewType === 1 ? <WeekView viewContent={viewContent} />
-                    : <DayView viewContent={viewContent} />
-            }
+    // eslint-disable-next-line react/jsx-key
+    const render = [<DateSelector viewContent={viewContent} setViewContent={setViewContent} selectedDate={selectedDate} setSelectedDate={setSelectedDate} />,
+    <>{ viewType === 0 || viewType === 1 ? <Days /> : <></> }</>];
 
-        </>
-    )
+    if(viewType === 0) {
+        render.push(<MonthView viewContent={viewContent} currentDate={currentDate} />);
+    } else if(viewType === 1) {
+        render.push(<WeekView viewContent={viewContent} />);
+    } else {
+        render.push(<DayView viewContent={viewContent} />);
+    }
+
+    render.push(<DetailScheduleModal />);
+
+    return <>{render}</>;
 }
