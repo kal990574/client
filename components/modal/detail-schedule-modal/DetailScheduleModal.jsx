bfb84649b40256692ce3a09daa3810ffd44025c2
kitez <duswl339@ajou.ac.kr>
2023-03-29 13:11:05 +0900

    feat: add Detail Schedule Modal

---
+++ components/modal/detail-schedule-modal/DetailScheduleModal.jsx
@@ -0,0 +1,15 @@
+import styles from './DetailScheduleModal.module.css';
+
+/**
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function DetailScheduleModal() {
+    return (
+        <section className={styles.container}>
+            <div className={styles.modalContainer}>
+
+            </div>
+        </section>
+    )
+}
