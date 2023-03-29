fa186da46a7f81b94266d3bc76f4b24727f59d76
kitez <duswl339@ajou.ac.kr>
2023-03-29 13:12:31 +0900

    style: delete import React

--- components/date-selector/DateSelector.jsx
+++ components/date-selector/DateSelector.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from './DateSelector.module.css';
 import { BiCaretDown, BiCalendar, BiBookBookmark } from "react-icons/bi";
 
