011236177ff611e3f2c289566a771d606500aabe
kitez <duswl339@ajou.ac.kr>
2023-03-31 18:23:40 +0900

    feat: add Detail Schedule Modal

--- components/modal/detail-schedule-modal/DetailScheduleModal.jsx
+++ components/modal/detail-schedule-modal/DetailScheduleModal.jsx
@@ -1,13 +1,18 @@
 import styles from './DetailScheduleModal.module.css';
+import '../../calendar/configs';
+import DATE from "../../calendar/configs";
 
 /**
  * @returns {JSX.Element}
  * @constructor
  */
-export default function DetailScheduleModal() {
+export default function DetailScheduleModal({dateString, setOpenModal}) {
+    const date = new Date(Date.parse(dateString));
+
     return (
-        <section className={styles.container}>
-            <div className={styles.modalContainer}>
+        <section className={styles.container} onClick={(e) => console.log(e.currentTarget)}>
+            <div className={styles.modalContainer} onClick={(e) => console.log(e.currentTarget)}>
+                <h1 className={styles.date}>{date.getMonth()+1+'월 '+date.getDate()+'일 '+DATE[date.getDay()]}</h1>
 
             </div>
         </section>
