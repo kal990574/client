a6d4296b01efaf400e9abaa7c2ee690c2c7d385c
mambasa <101706131+kal990574@users.noreply.github.com>
2023-03-31 22:18:41 +0900

    feat: update viewschedule navigationtop

--- components/nav-top-viewSchedule/NavigationTop.jsx
+++ components/nav-top-viewSchedule/NavigationTop.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import styles from './NavigationTop.module.css';
 import { BiMenu } from "react-icons/bi";
-import { HiX }from "react-icons/hi";
+import { HiChatAlt2, HiX }from "react-icons/hi";
 
 export default function NavigationTop({open, setOpen, viewType, setViewType}) {
     return (
+        <>
         <section className={styles.container}>
             <div>
-                <div>일정 세부사항</div>
-                <BiMenu class={styles.icon} />
-                <HiX class={styles.icon} />
-            </div>
-            <div className={styles.iconContainer}>
+                <HiX className={styles.icon} />
+                <BiMenu className={styles.iconRight} />
             </div>
         </section>
+        <h2 className={styles.title}>일정 세부사항</h2>
+        </>
     );
 }
