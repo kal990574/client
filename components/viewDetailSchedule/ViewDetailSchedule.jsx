ed84ff4488fe9ff15a142575f10108716e331091
mambasa <101706131+kal990574@users.noreply.github.com>
2023-03-31 22:57:57 +0900

    feat: update dummy data in viewDetailSchedule

--- components/viewDetailSchedule/ViewDetailSchedule.jsx
+++ components/viewDetailSchedule/ViewDetailSchedule.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './ViewDetailSchedule.module.css';
+import {DETAIL_SCHEDULE} from "../../common/dummy";
 export default function NavigationTop({open, setOpen}) {
     return (
         <>
-        <div>벚꽃 보러가기</div>
+        <div style={{background:`${DETAIL_SCHEDULE[0].color}`}} className={styles.color}></div>
+        <div>{DETAIL_SCHEDULE[0].title}</div>
+        <div>세부사항</div>
+        <div>{DETAIL_SCHEDULE[0].timeStampS} ~ {DETAIL_SCHEDULE[0].timeStampE}</div>
         <div>장소</div>
         <div>인원</div>
+        <div>Line</div>
         <div>좋아요</div>
         </>
     );
