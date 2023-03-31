99c534bf64a20db9e1643fe88ae07bafb33321a3
kitez <duswl339@ajou.ac.kr>
2023-03-31 18:25:39 +0900

    refactor: hide dates that do not fall within the selected month

--- components/view/month-view/MonthView.jsx
+++ components/view/month-view/MonthView.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './MonthView.module.css';
 import {addDays, endOfMonth, endOfWeek, format, startOfMonth, startOfWeek} from "date-fns";
+import DetailScheduleModal from "../../modal/detail-schedule-modal/DetailScheduleModal";
 
-export default function MonthView({viewContent, currentDate, selectedDate, onDateClick}) {
+export default function MonthView({setDay, viewContent, currentDate, selectedDate, onDateClick}) {
     const monthStart = startOfMonth(currentDate);
     const monthEnd = endOfMonth(monthStart);
     const startDate = startOfWeek(monthStart);
     const endDate = endOfWeek(monthEnd);
     const today = new Date();
+
+    const onClickEvent = (e) => {
+        console.log(e.target.id);
+        setDay(e.target.id);
+    }
 
     const rows = [];
     let days = [];
@@ -21,21 +27,26 @@ export default function MonthView({viewContent, currentDate, selectedDate, onDat
         for(let i = 0;  i < 7 ; i++) {
             key++;
             formattedDate = format(day, 'd');
-            const cloneDay = day;
-            if(today.getFullYear() === day.getFullYear() && today.getMonth() === day.getMonth() && today.getDate() === day.getDate() ) {
-                days.push(
-                    <div className={`${styles.days} ${styles.today}`} key={key}>
-                        <span className={i === 0 ? styles.sun : i === 6 ? styles.sat : styles.date}>{formattedDate}</span>
-                    </div>,
-                );
+            if(day.getMonth() === currentDate.getMonth()) {
+                if(today.getFullYear() === day.getFullYear() && today.getMonth() === day.getMonth() && today.getDate() === day.getDate() ) {
+                    days.push(
+                        <div onClick={onClickEvent} id={day.getFullYear()+'-'+(day.getMonth()+1)+'-'+day.getDate()} className={`${styles.days} ${styles.today}`} key={key}>
+                            <span id={day.getFullYear()+'-'+(day.getMonth()+1)+'-'+day.getDate()} className={i === 0 ? styles.sun : i === 6 ? styles.sat : styles.date}>{formattedDate}</span>
+                        </div>,
+                    );
+                } else {
+                    days.push(
+                        <div id={day.getFullYear()+'-'+(day.getMonth()+1)+'-'+day.getDate()} key={key} onClick={onClickEvent} className={styles.days}>
+                            <span id={day.getFullYear()+'-'+(day.getMonth()+1)+'-'+day.getDate()} className={i === 0 ? styles.sun : i === 6 ? styles.sat : styles.date}>{formattedDate}</span>
+                        </div>,
+                    );
+                }
             } else {
                 days.push(
                     <div className={styles.days} key={key}>
-                        <span className={i === 0 ? styles.sun : i === 6 ? styles.sat : styles.date}>{formattedDate}</span>
                     </div>,
                 );
             }
-
             day = addDays(day, 1);
         }
         rows.push(
