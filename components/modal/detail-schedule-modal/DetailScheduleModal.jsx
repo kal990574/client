cede09838bdedb2d96f71d0890c19c1065dac916
kitez <duswl339@ajou.ac.kr>
2023-03-31 19:08:36 +0900

    feat: add DetailScheduleCard Component in DetailScheduleModal

--- components/modal/detail-schedule-modal/DetailScheduleModal.jsx
+++ components/modal/detail-schedule-modal/DetailScheduleModal.jsx
@@ -1,6 +1,8 @@
 import styles from './DetailScheduleModal.module.css';
 import '../../calendar/configs';
 import DATE from "../../calendar/configs";
+import {DETAIL_SCHEDULE} from "../../../common/dummy";
+import DetailScheduleCard from "../../card/detail-schedule-card/DetailScheduleCard";
 
 /**
  * @returns {JSX.Element}
@@ -13,7 +15,11 @@ export default function DetailScheduleModal({dateString, setOpenModal}) {
         <section className={styles.container} onClick={(e) => console.log(e.currentTarget)}>
             <div className={styles.modalContainer} onClick={(e) => console.log(e.currentTarget)}>
                 <h1 className={styles.date}>{date.getMonth()+1+'월 '+date.getDate()+'일 '+DATE[date.getDay()]}</h1>
-
+                {
+                    DETAIL_SCHEDULE.map((item) => {
+                        return <DetailScheduleCard props={item}>{item.title}</DetailScheduleCard>
+                    })
+                }
             </div>
         </section>
     )
