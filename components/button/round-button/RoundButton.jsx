4edf6e79fea2b3f7ed8d62442df2b9711f6a41e5
kitez <duswl339@ajou.ac.kr>
2023-03-31 19:25:52 +0900

    feat: add RoundButton

---
+++ components/button/round-button/RoundButton.jsx
@@ -0,0 +1,10 @@
+import styles from './RoundButton.module.css';
+
+export default function RoundButton({onClickEvent, src}) {
+    return (
+        <button className={styles.container}>
+            <div />
+            <div />
+        </button>
+    )
+}
