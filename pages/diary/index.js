e7a5ce8000a5d68b371fd24e5cbca9c8d196f438
kitez <duswl339@ajou.ac.kr>
2023-03-31 18:41:57 +0900

    refactor: delete diary and memo pages

--- pages/diary/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Diary() {
-    return (
-        <div>
-            hello diary page
-        </div>
-    );
-}
--- pages/memo/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Memo() {
-    return (
-        <div>
-            hello memo page
-        </div>
-    );
-}
