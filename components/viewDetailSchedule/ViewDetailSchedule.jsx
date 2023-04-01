ac9d2e3ff6c810543912520bdd91f5802fb4d149
mambasa <101706131+kal990574@users.noreply.github.com>
2023-04-01 20:45:42 +0900

    feat: update viewDetailSchedule css

--- components/viewDetailSchedule/ViewDetailSchedule.jsx
+++ components/viewDetailSchedule/ViewDetailSchedule.jsx
@@ -3,15 +3,20 @@ import styles from './ViewDetailSchedule.module.css';
 import {DETAIL_SCHEDULE} from "../../common/dummy";
 export default function NavigationTop({open, setOpen}) {
     return (
-        <>
-        <div style={{background:`${DETAIL_SCHEDULE[0].color}`}} className={styles.color}></div>
-        <div className={styles.title}>{DETAIL_SCHEDULE[0].title}</div>
-        <div className={styles.detail}>세부사항</div>
-        <div>{DETAIL_SCHEDULE[0].timeStampS} ~ {DETAIL_SCHEDULE[0].timeStampE}</div>
-        <div>장소</div>
-        <div>인원</div>
-        <div>Line</div>
-        <div>좋아요</div>
-        </>
+        <section className={styles.container}>
+            <div className={styles.top}>
+                <div style={{background:`${DETAIL_SCHEDULE[0].color}`}} className={styles.color}></div>
+                <div className={styles.title}>{DETAIL_SCHEDULE[0].title}</div>
+            </div>
+            <div className={styles.detail}>오랜만에 데이트</div>
+            <div className={`${styles.middle} ${styles.item}`}>
+                <div>{DETAIL_SCHEDULE[0].timeStampS}</div>
+                <div>{DETAIL_SCHEDULE[0].timeStampE}</div>
+            </div>
+            <div className={styles.item}>장소</div>
+            <div className={styles.item}>인원</div>
+            <div className={styles.line}></div>
+            <div className={styles.item}>좋아요</div>
+        </section>
     );
 }
