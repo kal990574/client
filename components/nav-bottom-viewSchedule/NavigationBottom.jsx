19cac75e23d8644f1b0994960034b11e02eafb00
mambasa <101706131+kal990574@users.noreply.github.com>
2023-04-01 20:58:13 +0900

    feat: add bottom navi

--- components/nav-bottom-viewSchedule/NavigationBottom.jsx
+++ components/nav-bottom-viewSchedule/NavigationBottom.jsx
@@ -4,8 +4,8 @@ import {DETAIL_SCHEDULE} from "../../common/dummy";
 
 export default function NavigationBottom({open, setOpen}) {
     return (
-        <>
+        <section className={styles.container}>
         <div style={{background:`${DETAIL_SCHEDULE[0].color}`}} className={styles.comment}></div>
-        </>
+        </section>
     );
 }
