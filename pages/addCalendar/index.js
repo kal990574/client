bc690519c2fcdbe2e2d4f67c261f4f777ab309d3
kitez <duswl339@ajou.ac.kr>
2023-04-03 04:34:59 +0900

    refactor: addCalendar page's structure

--- pages/addCalendar/index.js
+++ pages/addCalendar/index.js
@@ -1,5 +1,5 @@
 import styles from './AddCalendar.module.css';
-import { BiX } from 'react-icons/bi';
+import { BiX, BiTime, BiPlusCircle } from 'react-icons/bi';
 
 export default function AddCalendar() {
     return (
@@ -11,29 +11,58 @@ export default function AddCalendar() {
             </div>
             <div className={styles.formContainer}>
                 {/* input */}
-                <div>
+                <div className={styles.lineInputContainer}>
                     {/* title */}
-                    title
+                    <input className={styles.lineInput} placeholder={"제목 추가( 필수 입력 )"}/>
                 </div>
-                <div>
+                <div className={styles.lineInputContainer}>
                     {/* content */}
-                    content
+                    <input className={styles.lineInput} placeholder={"내용 추가"} />
                 </div>
                 <div>
                     {/* color */}
-                    color
+                    <div>색상</div>
+                    <div></div>
                 </div>
                 <div>
                     {/* date */}
-                    date
+                    <div>
+                        <div>기간</div>
+                        <BiTime />
+                        <div>toggle</div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                start date
+                            </div>
+                            <div>
+                                start time
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                end date
+                            </div>
+                            <div>
+                                end time
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     {/* place */}
-                    place
+                    <div>장소</div>
+                    <div className={styles.lineInputContainer} style={{height: '26px'}}>
+                        <input className={styles.lineInput} style={{fontSize: '16px'}} />
+                    </div>
                 </div>
                 <div>
                     {/* add friends */}
-                    add friends
+                    <div>
+                        일정을 함께할 친구를 추가
+                    </div>
+                    <BiPlusCircle />
                 </div>
             </div>
         </main>
