73d6a2ed75c1c9ee51e932225492989e0b36fbd6
kitez <duswl339@ajou.ac.kr>
2023-04-04 11:05:56 +0900

    feat: add color palette modal

---
+++ components/modal/color-palette-modal/ColorPaletteModal.jsx
@@ -0,0 +1,33 @@
+import styles from './ColorPaletteModal.module.css';
+import {Wheel} from "@uiw/react-color";
+import {useState} from "react";
+import { BiX } from 'react-icons/bi';
+
+export default function ColorPaletteModal({close}) {
+    const [hex, setHex] = useState("#ffff");
+    const [category, setCategory] = useState('카테고리 이름');
+
+    const onChangeCategory = (e) => {
+        setCategory(e.currentTarget.value);
+    }
+
+    return (
+        <>
+            <div className={styles.layer} onClick={close} />
+            <div className={styles.modal}>
+                <div className={styles.topContainer}>
+                    <div className={styles.color} style={{background: hex}} />
+                    <input defaultValue={category} type={"text"} onChange={onChangeCategory}  className={styles.categoryName} />
+                    <BiX className={styles.closeIcon} onClick={close} />
+                </div>
+                <div className={styles.colorPaletteContainer}>
+                    <Wheel color={hex} onChange={(color) => {
+                        setHex(color.hex);
+                    }}/>
+                    <span className={styles.hex}>{hex}</span>
+                </div>
+                <button className={styles.button}>추가</button>
+            </div>
+        </>
+    );
+}
