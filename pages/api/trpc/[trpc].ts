65507d2e18cd5a2481458efc051714d6772b7856
Jin Hyeok Son <thswlsgur98@gmail.com>
2023-04-16 11:12:46 +0900

    feat: trpc + prisma

---
+++ pages/api/trpc/[trpc].ts
@@ -0,0 +1,22 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { env } from "~/env.mjs";
+import { createTRPCContext } from "~/server/api/trpc";
+import { appRouter } from "~/server/api/root";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  onError: ({ path, error }) => {
+      if (env.NODE_ENV === "development") {
+          console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+          );
+      }
+
+      if (error.code === "UNAUTHORIZED") {
+
+      }
+  }
+});
