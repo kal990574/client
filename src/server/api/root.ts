67067c2fbc65a97fbdcdc332256c1d24629def67
Son Jin Hyeok <thswlsgur98@gmail.com>
2023-05-17 18:09:39 +0900

    feat: Schedule & Group API

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
-import {scheduleRouter} from "~/server/api/routers/schedule";
+import { scheduleRouter } from "~/server/api/routers/schedule";
+import { groupRouter } from "~/server/api/routers/group";
 
 /**
  * This is the primary router for your server.
@@ -8,8 +8,8 @@ import {scheduleRouter} from "~/server/api/routers/schedule";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   schedule: scheduleRouter,
+  group: groupRouter,
 });
 
 // export type definition of API
