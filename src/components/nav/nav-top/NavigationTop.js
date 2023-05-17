39f8c1900dd862efd4d1a257161636d58b0b754b
kitez <duswl339@ajou.ac.kr>
2023-05-17 17:41:54 +0900

    feat: add go Mypage function

--- src/components/nav/nav-top/NavigationTop.js
+++ src/components/nav/nav-top/NavigationTop.js
@@ -2,8 +2,14 @@ import React from 'react';
 import styles from './NavigationTop.module.css';
 import { BiBell, BiUserCircle, BiMenu } from "react-icons/bi";
 import ViewSelector from "../../view-selector/ViewSelector";
+import {useRouter} from "next/router";
 
 export default function NavigationTop({open, setOpen, viewType, setViewType}) {
+    const router = useRouter();
+    const goMyPage = () => {
+        router.push('/mypage');
+    }
+
     return (
         <section className={styles.container}>
             <div>
@@ -13,8 +19,8 @@ export default function NavigationTop({open, setOpen, viewType, setViewType}) {
                 {
                     viewType !== undefined && <ViewSelector open={open} setOpen={setOpen} viewType={viewType} setViewType={setViewType} />
                 }
-                <BiBell className={styles.icon} />
-                <BiUserCircle className={styles.icon} />
+                <BiBell className={styles.icon}  />
+                <BiUserCircle className={styles.icon} onClick={goMyPage}/>
             </div>
         </section>
     );
