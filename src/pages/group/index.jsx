ef2a89a66060b0a3fcd1c3cb951e862408de9c54
kitez <duswl339@ajou.ac.kr>
2023-05-17 16:10:21 +0900

    feat: Group Page

--- src/pages/group/index.jsx
+++ src/pages/group/index.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CustomHead from "../../components/head/CustomHead";
+import NavigationTop from "../../components/nav/nav-top/NavigationTop";
+import NavigationBottom from "../../components/nav/nav-bottom/NavigationBottom";
+import styles from './Group.module.css';
+import {BsFillPlusCircleFill} from 'react-icons/bs'
+import GroupCard from "../../components/group/group-card/GroupCard";
+
+const dummy = [
+    {
+        primaryColor: '#FDE4F7',
+        FontColor: '#FE4545',
+        InviteColor: '#FA9B9B',
+        groupName: '사이버보안 캡스톤디자인 스터디',
+        memberList: ['손진혁', '이재현', '장연지'],
+        content: '사이버보안 캡스톤디자인 스터디입니다.'
+    },
+    {
+        primaryColor: '#FFFDCF',
+        FontColor: '#FEB445',
+        InviteColor: '#FAE59B',
+        groupName: '해머',
+        memberList: ['손진혁', '이재현', '장연지', '한창민'],
+        content: '사이버보안학과 해머'
+    },
+]
 
 export default function Group() {
     return (
-        <main>
-            group page
+        <main className={styles.pageContainer}>
+            <CustomHead title={'Group'} content={'GroupPage'}/>
+            <NavigationTop />
+            <section className={styles.sectionContainer}>
+                <div className={styles.titleContainer}>
+                    <h1>MY Group</h1>
+                    <BsFillPlusCircleFill className={styles.icon} />
+                </div>
+                <div className={styles.groupListContainer}>
+                    <div className={styles.groupListInnerContainer}>
+                        {
+                            dummy.map(d => {
+                                return <GroupCard content={d.content} groupName={d.groupName} FontColor={d.FontColor} InviteColor={d.InviteColor} memberList={d.memberList} primaryColor={d.primaryColor} />
+                            })
+                        }
+                    </div>
+                </div>
+            </section>
+            <NavigationBottom />
         </main>
     );
 }
