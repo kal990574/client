5f1c9a75888b3a4cba9e7ffa950df7cbe4ea947c
kitez <duswl339@ajou.ac.kr>
2023-05-19 16:58:38 +0900

    refactor: modify top-nav

--- src/components/nav/nav-top/NavigationTop.js
+++ src/components/nav/nav-top/NavigationTop.js
@@ -3,15 +3,22 @@ import styles from './NavigationTop.module.css';
 import { BiBell, BiUserCircle, BiMenu } from "react-icons/bi";
 import ViewSelector from "../../view-selector/ViewSelector";
 import {signIn} from "next-auth/react";
+import {useRouter} from "next/router";
 
 export default function NavigationTop({open, setOpen, viewType, setViewType}) {
+    const router = useRouter();
+    const path = router.pathname;
+    console.log(path)
+
     return (
         <section className={styles.container}>
             <div>
                 <BiMenu className={styles.icon} />
             </div>
             <div className={styles.iconContainer}>
-                <ViewSelector open={open} setOpen={setOpen} viewType={viewType} setViewType={setViewType} />
+                {
+                    path === '/' ? <ViewSelector open={open} setOpen={setOpen} viewType={viewType} setViewType={setViewType} /> : <></>
+                }
                 <BiBell class={styles.icon} />
                 <BiUserCircle onClick={() => signIn("kakao")} class={styles.icon} />
             </div>
