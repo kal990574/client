fd13b150941c1c50cddf4e6b92f070a7deca840d
kitez <duswl339@ajou.ac.kr>
2023-05-19 21:10:33 +0900

    feat: add kakao login button

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from '../styles/Main.module.css';
+import {useRouter} from "next/router";
 
 export default function Main({session}) {
-    console.log("Main session: ", session)
+    console.log("Main session: ", session);
+    const router = useRouter();
+    const onClickKakao = async () => {
+        const res = await fetch('http://localhost:3000/api/auth/callback/kakao').then((response) => {
+            console.log(response);
+            router.replace(response.url)
+        }).catch((err) => {
+           console.log(err);
+        });
+
+        console.log(res);
+    }
+
     return(
         <main className={styles.container}>
-            home
+            <h1 className={styles.logo}>CALENDARY</h1>
+            <img onClick={onClickKakao} className={styles.kakao} src={'/kakao_login.png'} />
         </main>
     );
 }
