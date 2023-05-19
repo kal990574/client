827a7c6cc106b99581a003ef479cfefe5a12ab4f
kitez <duswl339@ajou.ac.kr>
2023-05-19 11:09:44 +0900

    feat: add my page edit page

---
+++ src/pages/mypage/edit/index.jsx
@@ -0,0 +1,17 @@
+import styles from './MyEdit.module.css';
+import CustomHead from "../../../components/head/CustomHead";
+import {useRouter} from "next/router";
+import MyNavigation from "../../../components/mypage/nav/my-navigation";
+
+export default function MyEdit() {
+    const router = useRouter();
+    console.log(router.pathname)
+    return (
+        <main>
+            <CustomHead title={'MyEdit'} content={'MyEditPage'}/>
+            <MyNavigation />
+
+
+        </main>
+    )
+}
