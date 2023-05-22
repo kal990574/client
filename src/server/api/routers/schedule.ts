553d1c49171ef5a66e8a40110f2bcaf92185b1f0
Son Jin Hyeok <thswlsgur98@gmail.com>
2023-05-22 09:42:40 +0900

    feat: api features

--- src/server/api/routers/schedule.ts
+++ src/server/api/routers/schedule.ts
@@ -7,11 +7,14 @@ import {
 } from "~/server/api/trpc";
 
 export const scheduleRouter = createTRPCRouter({
+  // 스케쥴 생성
   createSchedule: protectedProcedure.input(z.object({
     title: z.string(),
     summary: z.string(),
+    categoryName: z.optional(z.string()),
     startDate: z.date(),
     startTime: z.optional(z.date()),
+    location: z.optional(z.string()),
     endDate: z.date(),
     endTime: z.optional(z.date()),
   })).mutation(async ({ ctx, input  }) => {
@@ -21,7 +24,6 @@ export const scheduleRouter = createTRPCRouter({
       data: {
         ...input,
         ownerId: userId,
-        location: "",
         participants: {
           create: {
             userId: userId
@@ -31,6 +33,7 @@ export const scheduleRouter = createTRPCRouter({
     });
   }),
 
+  // 참가되어 있는 모든 스케줄 조회
   getSchedules: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.schedule.findMany({
       where: {
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -7,6 +7,42 @@ import {
 } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
+    // 특정 사용자 정보 조회
+    getUserInfo: protectedProcedure.input(z.object({
+        id: z.string(),
+    })).query(async ({ ctx, input  }) => {
+        const userId = ctx.session.user.id;
+
+        const followerCount = await ctx.prisma.following.count({
+            where: {
+                followeeId: input.id
+            }
+        });
+
+        const followingCount = await ctx.prisma.following.count({
+            where: {
+                followerId: input.id
+            }
+        });
+
+        const userInfo = await ctx.prisma.user.findUniqueOrThrow({
+            where: {
+                id: input.id,
+            }
+        });
+
+        return {
+            ...userInfo,
+            followerCount: followerCount,
+            followingCount: followingCount,
+        }
+    }),
+    createCategory: protectedProcedure.input(z.object({
+        name: z.string()
+    })).mutation(async ({ ctx, input  }) => {
+
+    }),
+    // 프로필 수정
     updateMyProfile: protectedProcedure.input(z.object({
         
     })).mutation(async ({ ctx, input  }) => {
@@ -22,6 +58,7 @@ export const userRouter = createTRPCRouter({
         })
     }),
 
+    // 팔로우 요청 전송
     sendFollowRequest: protectedProcedure.input(z.object({
         id: z.string()
     })).mutation(async ({ ctx, input  }) => {
@@ -35,7 +72,8 @@ export const userRouter = createTRPCRouter({
         })
     }),
 
-    acceptFollowRequest: protectedProcedure.input(z.object({
+    // 팔로우 요청 액션
+    replyFollowRequest: protectedProcedure.input(z.object({
         id: z.string()
     })).mutation(async ({ ctx, input  }) => {
         const userId = ctx.session.user.id;
@@ -46,46 +84,4 @@ export const userRouter = createTRPCRouter({
             }
         });
     }),
-
-    denyFollowRequest: protectedProcedure.input(z.object({
-        id: z.string()
-    })).mutation(async ({ ctx, input  }) => {
-        const userId = ctx.session.user.id;
-
-        const receivedRequest = await ctx.prisma.followRequest.findUniqueOrThrow({
-            where: {
-                id: input.id
-            }
-        });
-    }),
-
-  getUserInfo: protectedProcedure.input(z.object({
-    id: z.string(),
-  })).query(async ({ ctx, input  }) => {
-    const userId = ctx.session.user.id;
-
-    const followerCount = await ctx.prisma.following.count({
-        where: {
-            followeeId: input.id
-        }
-    });
-
-    const followingCount = await ctx.prisma.following.count({
-        where: {
-            followerId: input.id
-        }
-    });
-
-    const userInfo = await ctx.prisma.user.findUniqueOrThrow({
-        where: {
-            id: input.id,
-        }
-    });
-
-    return {
-        ...userInfo,
-        followerCount: followerCount,
-        followingCount: followingCount,
-    }
-  }),
 });
