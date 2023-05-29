68ca9151c916790cb85e2dafd95d6f6c196d36d1
kitez <duswl339@ajou.ac.kr>
2023-05-29 20:33:14 +0900

    refactor: add group page's category select changes color select

--- src/components/calendar/configs.js
+++ src/components/calendar/configs.js
@@ -2,4 +2,14 @@ const DATE = [
     '일', '월', '화', '수', '목', '금', '토'
 ];
 
-export default DATE;
+const PRIMARY_COLOR = {
+    'BLUE': {primary: '#001aff', sub: '#4d5eff', font: '#ffffff'},
+    'RED': {primary: '#ff0000', sub: '#ff6969', font: '#ffffff'},
+    'ORANGE': {primary: '#ff6f00', sub: '#ffad6e', font: '#ffffff'},
+    'YELLOW': {primary: '#ffe100', sub: '#fff185', font: '#a39000'},
+    'GREEN' : {primary: '#007a04', sub: '#71bf73', font: '#ffffff'},
+    'VIOLET' : {primary: '#7c00ad', sub: '#da7dff', font: '#ffffff'},
+    'GRAY' : {primary: '#454545', sub: '#949494', font: '#ffffff'},
+}
+
+export {DATE, PRIMARY_COLOR};
